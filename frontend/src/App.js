368a028cf17dcdda7ad419d83580b085040a9bd8
Kirubakaran Periyasamy <88470706+kiruba11k@users.noreply.github.com>
2026-03-09 16:25:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SalesNav from "./pages/SalesNav";
-import Requests from "./pages/Requests";
 import Sidebar from "./components/Sidebar";
+import SalesNav from "./pages/SalesNav";
+import GoogleAgent from "./pages/GoogleAgent";
+import Enrichment from "./pages/Enrichment";
+import Requests from "./pages/Requests";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <div style={{flex:1,padding:20}}>
           <Routes>
             <Route path="/" element={<SalesNav />} />
+            <Route path="/google" element={<GoogleAgent />} />
+            <Route path="/enrichment" element={<Enrichment />} />
             <Route path="/requests" element={<Requests />} />
           </Routes>
         </div>
