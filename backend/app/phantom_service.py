e7a2c9ad6e38ec19c4479f25b8f88af83636c7e5
Kirubakaran Periyasamy <88470706+kiruba11k@users.noreply.github.com>
2026-03-10 14:21:08 +0530

    Update phantom_service.py

--- backend/app/phantom_service.py
+++ backend/app/phantom_service.py
@@ -2,14 +2,16 @@ import requests
 import os
 
 PHANTOM_API_KEY = os.getenv("PHANTOM_API_KEY")
-PHANTOM_AGENT_ID = os.getenv("PHANTOM_AGENT_ID")
-
-BASE_URL = "https://api.phantombuster.com/api/v2"
 
 headers = {
     "X-Phantombuster-Key-1": PHANTOM_API_KEY,
     "Content-Type": "application/json"
 }
+
+BASE_URL = "https://api.phantombuster.com/api/v2"
+
+SEARCH_AGENT_ID = os.getenv("PHANTOM_SEARCH_AGENT_ID")
+SCRAPER_AGENT_ID = os.getenv("PHANTOM_AGENT_ID")
 
 def launch_agent(filters):
     url = f"{BASE_URL}/agents/launch"
@@ -29,3 +31,38 @@ def fetch_container_output(container_id):
     url = f"{BASE_URL}/containers/fetch-output"
     response = requests.get(url, headers=headers, params={"id": container_id})
     return response.json()
+
+def launch_company_search(search_url):
+
+    payload = {
+        "id": SEARCH_AGENT_ID,
+        "argument": {
+            "search": search_url
+        }
+    }
+
+    r = requests.post(
+        f"{BASE_URL}/agents/launch",
+        json=payload,
+        headers=headers
+    )
+
+    return r.json()
+
+
+def launch_company_scraper(company_urls):
+
+    payload = {
+        "id": SCRAPER_AGENT_ID,
+        "argument": {
+            "linkedInCompanyPages": company_urls
+        }
+    }
+
+    r = requests.post(
+        f"{BASE_URL}/agents/launch",
+        json=payload,
+        headers=headers
+    )
+
+    return r.json()
