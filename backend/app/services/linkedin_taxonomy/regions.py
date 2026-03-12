e3cef8a882478e4e0cd77e9ae93c29b38c6d2330
Kirubakaran Periyasamy <88470706+kiruba11k@users.noreply.github.com>
2026-03-12 11:02:31 +0530

    Create regions.py

---
+++ backend/app/services/linkedin_taxonomy/regions.py
@@ -0,0 +1,19 @@
+REGION_MAP = {
+    "Global": "90000000",
+    "United States": "103644278",
+    "United Kingdom": "101165590",
+    "Canada": "101174742",
+    "Germany": "101282230",
+    "France": "105015875",
+    "India": "102713980",
+    "Australia": "101452733",
+    "Netherlands": "102890719",
+    "Singapore": "102454443",
+    "United Arab Emirates": "104305776",
+    "Brazil": "106057825",
+    "Israel": "101620216",
+    "Japan": "101355337",
+    "Spain": "105646813",
+    "Italy": "103350119",
+    "Mexico": "103323778"
+}
