c65f9de409fcdb88de055b4e0d6368012ea2069f
Kirubakaran Periyasamy <88470706+kiruba11k@users.noreply.github.com>
2026-03-17 15:15:23 +0530

    Create stream_manager.py

---
+++ backend/app/services/stream_manager.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+streams = defaultdict(list)
+
+def push_update(request_id, data):
+    streams[request_id].append(data)
+
+def get_updates(request_id):
+    updates = streams[request_id][:]
+    streams[request_id] = []
+    return updates
